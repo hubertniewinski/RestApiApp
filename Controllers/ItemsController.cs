f15a7b58032106c3c8b39132429f54cc22c8c77a
Hubert Niewiński <hubertniewinski@yahoo.com>
2021-10-16 16:47:53 +0200

    http post, put , delete

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -36,5 +36,52 @@ namespace Catalog.Controllers
 
             return item.AsDto();
         }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new() {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id}, item.AsDto());
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid item, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(item);
+
+            if(existingItem == null)
+                return NotFound();
+
+            Item newItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            repository.UpdateItem(newItem);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if (existingItem == null)
+                return NotFound();
+
+            repository.DeleteItem(id);
+
+            return NoContent();
+        }
     }
 }
