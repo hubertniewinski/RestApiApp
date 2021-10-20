7259a9eb21771d9680c41b7e5dfec2ab742a63ca
Hubert Niewiński <hubertniewinski@yahoo.com>
2021-10-20 23:15:04 +0200

    added asynchronous methods

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -1,18 +1,19 @@
 ﻿using Catalog.Entities;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Catalog.Repositories
 {
     public interface IItemsRepository
     {
-        Item GetItem(Guid id);
-        IEnumerable<Item> GetItems();
+        Task<Item> GetItemAsync(Guid id);
+        Task<IEnumerable<Item>> GetItemsAsync();
 
-        void CreateItem(Item item);
+        Task CreateItemAsync(Item item);
 
-        void UpdateItem(Item item);
+        Task UpdateItemAsync(Item item);
 
-        void DeleteItem(Guid id);
+        Task DeleteItemAsync(Guid id);
     }
 }
